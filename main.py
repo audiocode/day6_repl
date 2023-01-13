caee869834fe7b95146537617a23eef3d7f03a93
Mark Renker <markkrenker@gmail.com>
2023-01-13 19:54:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+username = input("Username > ")
+password = input("Password > ")
+if username == "mark" and password == "password":
+  print("Welcome Mark. Have a great day and remember to stay hydrated!")
+elif username == "sally" and password == "password":
+  print("Hey there, Sally! Today will be a great day of focus and productivity!")
+elif username == "jane" and password == "password":
+  print("Hi Jane. Here's to another day of joy and happiness!")
+else:
+  print("Go away!")
